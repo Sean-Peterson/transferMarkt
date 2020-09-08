91fcceb60c4a234777bb3840fa370e9757da57bd
Sean Peterson <seanpeterson11@gmail.com>
2020-09-07 21:53:11 -0500

    add base code that pulls from transfermarkt.com. It was getting blocked, but I was able to get around by changing the headers and using the requests library.

--- script.py
+++ script.py
@@ -1,13 +1,41 @@
-import urllib
-from bs4 import BeautifulSoup
+import bs4 as bs
+import urllib.request
+import lxml
 import xlwt
 from xlwt import Workbook
 import time
+import requests
+# baseUrl = "https://www.transfermarkt.com/"
+# page=ur.urlopen(baseUrl)
+# soup=BeautifulSoup(page, 'lxml')
+# transferMarktTest=soup.find_all('td')
+# # this is an array that has all of the brands included in the GSMarena DB. This array will be used to append to gsmarena.com/
+# baseUrlArray_a_tags = []
+# url = 'https://www.transfermarkt.com/aktuell/newsarchiv'
+# headers = {'User-Agent': 'Mozilla/5.0'}
+# content = urllib.request.urlopen(url).read()
+# soup = bs.BeautifulSoup(content,'lxml')
+# test = soup.title
+# print(test)
+url = 'https://www.transfermarkt.com/aktuell/newsarchiv'
+headers = {'User-Agent': 'Mozilla/5.0'}
+result = requests.get(url,headers=headers)
+c = result.content
+soup = bs.BeautifulSoup(c,'lxml')
+print(soup.title)
 
-allBrandsURL = "https://www.gsmarena.com/makers.php3"
-page=urllib.urlopen(allBrandsURL)
-soup=BeautifulSoup(page, 'lxml')
-allPhoneBrands=soup.find_all('td')
-# this is an array that has all of the brands included in the GSMarena DB. This array will be used to append to gsmarena.com/
-allBrandsURLArray = []
- 
+
+
+
+
+
+
+
+
+# practice stuff to test if it was actually working. Found out I needed to download lxml
+# import bs4 as bs
+# import urllib.request
+#
+# source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+# soup = bs.BeautifulSoup(source,'lxml')
+# print(soup.title)
