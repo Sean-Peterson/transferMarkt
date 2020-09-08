1f19560e4f29233782de21a4dc25d6cc2df1744d
Sean Peterson <seanpeterson11@gmail.com>
2020-09-07 20:47:29 -0500

    First commit with script and the base of bSoup

---
+++ script.py
@@ -0,0 +1,12 @@
+import urllib
+from bs4 import BeautifulSoup
+import xlwt
+from xlwt import Workbook
+import time
+
+allBrandsURL = "https://www.gsmarena.com/makers.php3"
+page=urllib.urlopen(allBrandsURL)
+soup=BeautifulSoup(page, 'lxml')
+allPhoneBrands=soup.find_all('td')
+# this is an array that has all of the brands included in the GSMarena DB. This array will be used to append to gsmarena.com/
+allBrandsURLArray = []
